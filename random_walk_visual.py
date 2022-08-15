d081162a10bab3d50a11f120f73ab51bfa914357
Munene254 <midwaymaster10@gmail.com>
2022-08-15 17:49:41 +0300

    Added style to walks

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -8,11 +8,17 @@ while True:
     # Make a random walk and plot the points
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+    
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    
     plt.show()
     
     keep_running = input("Make another walk? (yes/no): ")
     if keep_running == 'no':
         break
-    
     
